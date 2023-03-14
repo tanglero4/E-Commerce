6398a043065eba5f29101f21e28ac8b49fbadb66
tanglero4 <96814274+tanglero4@users.noreply.github.com>
2023-03-13 22:40:35 -0400

    Added basic models

--- Develop/models/Tag.js
+++ Develop/models/Tag.js
@@ -1,12 +1,18 @@
 const { Model, DataTypes } = require('sequelize');
-
 const sequelize = require('../config/connection.js');
 
 class Tag extends Model {}
-
 Tag.init(
   {
-    // define columns
+    id:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    tag_name:{
+      type: DataTypes.STRING
+    }
   },
   {
     sequelize,
@@ -16,5 +22,6 @@ Tag.init(
     modelName: 'tag',
   }
 );
+
 
 module.exports = Tag;
--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const routes = require('./routes');
-// import sequelize connection
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -9,8 +8,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
-
-// sync sequelize models to the database, then turn on the server
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
 });
